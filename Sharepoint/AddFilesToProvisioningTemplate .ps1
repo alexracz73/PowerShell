4dc4e7081e3d31c6130d7191c382ff477e776218
Alex <alexracz73@gmail.com>
2020-01-01 14:01:30 +0200

    Create AddFilesToProvisioningTemplate .ps1

---
+++ Sharepoint/AddFilesToProvisioningTemplate .ps1
@@ -0,0 +1,14 @@
+﻿   $Folders = @('Documents','Scripts')
+   $xmlLocation = 'c:\onedrive\template.pnp'
+   $FoldersLocation = "C:\onedrive"
+
+      $Folders | ForEach-Object {
+   $path = $FoldersLocation + "\" + $_;
+   (dir $path -Recurse) | %{
+  try{
+    $FName = "{site}" + $_.Directory.FullName.Substring($FoldersLocation.Length)
+    Add-PnPFileToProvisioningTemplate -Path $xmlLocation -Source $_.FullName -Folder $FName -Container $_.Directory.FullName  -ErrorAction Ignore
+  }
+ catch{}
+ }
+}
